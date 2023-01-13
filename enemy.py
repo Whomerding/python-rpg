26d0b6233c71c7e2219d98b7441956acb2368a51
Whomerding <wendy.homerding@gmail.com>
2023-01-13 09:29:49 -0500

    fix dictionary, choos attack and display attack method in player class

--- enemy.py
+++ enemy.py
@@ -1,7 +1,38 @@
 
 from player import Player
-
+import random
 class Enemy (Player):
-    def __init__():
+    def __init__(self):
         super ().__init__()
+        self.enemy_list = ["Hera", "The Cyclops", "The Minotaur", "Modern Life"]
+        self.enemy_name = random.choice (self.enemy_list)
         
+    """""
+    def hera (self):
+        if self.enemy_name == "Hera":
+            self.health += 5
+        if self.attack_name == "THE FURY OF THE GODS!!!!":
+            self.health -= 30
+            print ("Zeuss stay out of this!!!")
+    def cyclops (self):
+        if self.enemy_name == "The Cyclops":
+            self.health += 3
+        if self.attack_name == "a rabid squirrel":
+            self.health -= 25
+            print ("Ahhhhh my one giant eye has contracted rabies!!!")
+    def minotaur (self):
+        if self.enemy_name == "The Minotaur":
+            self.health += 2
+        if self.attack_name == "a small horse":
+            self.health -= 20
+    def modern_life (self):
+        if self.enemy_name == "Modern Life":
+            self.health -= 1
+        if self.attack_name == "your mama!":
+            self.health -= 30
+    def enemy_power_weakness (self):
+        self.hera ()
+        self.cyclops ()
+        self.minotaur ()
+        self.modern_life ()
+"""
--- player.py
+++ player.py
@@ -2,18 +2,19 @@ import random
 class Player:
     def __init__(self):
         self.health = 100
-        self.attack_power = random.randint (0, 15)
-        self.attack_name = {"attack": "THE FURY OF THE GODS!!!!", "attack": "Full on beast mode!", "attack": "Loosing your mind up in here! up in here!", "attack": "a very heavy rock", "attack": "a small horse", "attack": "your mama!", "attack": "a rabid squirrel"}
+        self.attack_name = {"THE FURY OF THE GODS!!!!": 25, "Full on beast mode!": 20, "Loosing your mind up in here! up in here!": 10, "a very heavy rock": 5, "a small horse": 10, "your mama!": 15, "a rabid squirrel": 12}
+        self.attack_power = int
 
     def change_attack_random (self):   
         self.attack_name = random.choice (list(self.attack_name()))
+        self.attack_name = random.choice (list (self.attack_name.value()))
 
     def choose_attack (self):
         self.display_attacks ()
         self.questions_herc_list = ["Mighty Hercules, how do you choose to attack this villanous fiend?", "How do you wish the dispel with this super evil bad guy?", "What attack would you like to use to kick this dudes butt?"]
-        self.random_question  = random.choice (f"{self.questions_herc_list}")
+        self.random_question  = random.choice (self.questions_herc_list)
         self.attack_name = input (self.random_question)
     
     def display_attacks (self):
-        for x in self.attack_name:
-            print (self.attack_name [x])
+        for attack in self.attack_name:
+            print (attack)
